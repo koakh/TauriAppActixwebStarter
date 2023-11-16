62aa1b072d4ad2968bdf23ac4a6b703a6ee1f6b5
Mário Monteiro <marioammonteiro@gmail.com>
2023-11-16 23:49:59 +0000

    mini change

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod server;
-
 use src_shared::rand;
 use std::thread;
+
+mod server;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
